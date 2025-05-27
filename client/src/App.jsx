cd761e4e9ee289dac58061a90e197a3b077de533
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-05-27 14:34:40 +0600

    Font Familly Add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import Marquee from "react-fast-marquee";
 
 export default function App() {
   return (
     <div>
-      <h1 className='text-3xl font-bold text-red-600 underline'>
-
-      Hello World
-      </h1>
+      <div className="bg-[#22863A]  p-2 text-white text-center text-xl font-bold ">
+        <Marquee>
+          আমাদের যে কোন পণ্য অর্ডার করতে কল বা WhatsApp করুন: +88010000000 | হট
+          লাইন: 000000-0000
+        </Marquee>
+      </div>
+      <h1 className="text-3xl font-bold text-red-600 underline">Hello World</h1>
     </div>
-  )
+  );
 }
