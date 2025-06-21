ac5be7844a9a11ff173d102d774c81de24b137ef
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-06-21 11:58:01 +0600

    Update all

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,44 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      <footer classname="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
-        <nav>
-          <h6 classname="footer-title">Services</h6>
-          <a classname="link link-hover">Branding</a>
-          <a classname="link link-hover">Design</a>
-          <a classname="link link-hover">Marketing</a>
-          <a classname="link link-hover">Advertisement</a>
-        </nav>
-        <nav>
-          <h6 classname="footer-title">Company</h6>
-          <a classname="link link-hover">About us</a>
-          <a classname="link link-hover">Contact</a>
-          <a classname="link link-hover">Jobs</a>
-          <a classname="link link-hover">Press kit</a>
-        </nav>
-        <nav>
-          <h6 classname="footer-title">Legal</h6>
-          <a classname="link link-hover">Terms of use</a>
-          <a classname="link link-hover">Privacy policy</a>
-          <a classname="link link-hover">Cookie policy</a>
-        </nav>
-        <form>
-          <h6 classname="footer-title">Newsletter</h6>
-          <fieldset classname="w-80">
-            <label>Enter your email address</label>
-            <div classname="join">
-              <input
-                type="text"
-                placeholder="username@site.com"
-                classname="input input-bordered join-item"
-              />
-              <button classname="btn btn-primary join-item">Subscribe</button>
-            </div>
-          </fieldset>
-        </form>
-      </footer>
-      
-    </div>
-  );
+    <div>Footer</div>
+  )
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,10 +3,11 @@ import Marquee from "react-fast-marquee";
 export default function Header() {
   return (
     <div>
-      <div className='bg-blue-400 text-white text-xl font-bold p-2'>
+      <div className="bg-teal-500 text-white text-xl font-bold p-2">
         <Marquee>
-          I can be a React component, multiple React components, or just some
-          text.
+          আপনার জীবনকে সহজতর করার জন্য, আমরা নিয়ে এসেছি "Online Store"।  এখানে
+          পাবেন আপনার প্রাত্যহিক প্রয়োজনীয় পণ্য থেকে শুরু করে বিশেষ দিনের
+          উপহার—সবই।
         </Marquee>
       </div>
     </div>
