2c4da33d736dc7e34340985a0e7379ccb392f250
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-06-21 12:14:05 +0600

    changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import Marquee from "react-fast-marquee";
 export default function Header() {
   return (
     <div>
-      <div className="bg-teal-500 text-white text-xl font-bold p-2">
+      <div className="bg-teal-500 text-white text-xl font-bold p-3">
         <Marquee>
        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, harum!
         </Marquee>
