cd24ec7e9b12dc207942810bc81c1b2373018895
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-06-21 12:17:41 +0600

    Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,8 @@ import Layout from './layout/Layout';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import Cart from './pages/Cart';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 
 export default function App() {
   const router = createBrowserRouter(
@@ -21,6 +23,8 @@ export default function App() {
             <Route index element={<Home />}></Route>
             <Route path="/shop" element={<Shop />}></Route>
             <Route path="/cart" element={<Cart />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/Signup" element={<SignUp />}></Route>
           </Route>
         </Route>
       </>
