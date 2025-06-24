a572304237d6edfc3d3b1c6380fa0a63d1cfdd67
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-06-24 11:52:59 +0600

    dfsa

--- client/src/components/Products/AllProducts.jsx
+++ client/src/components/Products/AllProducts.jsx
@@ -8,7 +8,7 @@ export default function AllProducts() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit,
         odio!
       </h2>
-      <h3>Lorem ipsum dolor sit amet consectetur adipisicing . Ad abf repellendus odio maxime maiores debitis nemo odit hic dicta sapiente </h3>
+      <h3>Lorem ipsum dolor sit amet consectetur adipisicing . Ad repellendus odio maxime maiores debitis nemo odit hic dicta sapiente </h3>
     </>
   );
 }
