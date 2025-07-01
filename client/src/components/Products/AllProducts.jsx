aef1c082cce769ddc9fcd54d332ac9fdf24ea36d
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-07-01 10:55:45 +0600

    sdfds

--- client/src/components/Products/AllProducts.jsx
+++ client/src/components/Products/AllProducts.jsx
@@ -5,7 +5,7 @@ export default function AllProducts() {
     <>
       <div>AllProduct22</div>
       <h2>
-        Lorem ipsum dolor consectetur  elit Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum minus eaque error repudiandae porro veritatis, nobis facilis sint recusandae culpa amet enim quo quisquam dolor, ab numquam itaque expedita eveniet!,
+        Lorem ipsum dolor consectetur  elit Lorem ipsum dolor sit amet consectetur  elit. Ipsum minus eaque error repudiandae porro veritatis, nobis facilis sint recusandae culpa amet enim quo quisquam dolor, ab numquam itaque expedita eveniet!,
       
       </h2>
       <div>
