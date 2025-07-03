d0f6100f27bd505055e24dc443c0a064789b386b
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-07-03 14:47:31 +0600

    dsafdsa

--- client/src/components/Products/AllProducts.jsx
+++ client/src/components/Products/AllProducts.jsx
@@ -9,7 +9,7 @@ export default function AllProducts() {
       
       </h2>
       <div>
-        <h1>Lorem, ipsum dolor sit amet  elit. Ad Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, eum! necessitatibus? <span></span></h1>
+        <h1>Lorem, ipsum dolor sit amet  elit. Ad Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, eum! necessitatibus? <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti ab et possimus velit nulla eligendi, quidem autem delectus doloremque adipisci ex tempora aliquid veritatis placeat. Expedita, veniam dolorum deleniti consequuntur fugiat nobis repudiandae veritatis et! Amet fugiat animi harum at, voluptates aliquam sit. Quo repellendus eaque architecto illum voluptate voluptatem ad inventore ipsa sapiente quia. Nobis ea blanditiis illum quis quibusdam reiciendis veritatis quae, ipsa repudiandae exercitationem! Atque quia iusto consectetur aut voluptatum libero saepe harum omnis, fugit minus ipsa laborum veniam! Non deserunt ipsum, quis nulla fugit saepe a quaerat sit temporibus obcaecati odio! Veritatis maiores commodi nostrum facilis.</span></h1>
       </div>
       <h3>Lorem ipsum dolor sit amet dfdsfds Ad repellendus odio  maiores debitis nemo odit hic dicta sapiente </h3>
     </>
